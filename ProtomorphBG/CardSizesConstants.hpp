63cbb40a30ec26271a177bb5a9505e68b0a1861d
Andrii <Andriyslisarchuk@gmail.com>
2018-09-11 00:13:27 +0300

    Added more standard sizes of cards

--- ProtomorphBG/CardSizesConstants.hpp
+++ ProtomorphBG/CardSizesConstants.hpp
@@ -9,20 +9,32 @@ struct CardSize {
     double height;
 };
 
-const std::array<CardSize, 13> sizes{
-    CardSize{63.5, 88},
-    CardSize{57, 89},
-    CardSize{56, 87},
-    CardSize{56, 100},
-    CardSize{59, 92},
-    CardSize{44, 68},
-    CardSize{43, 65},
+const std::array<CardSize, 25> sizes{
     CardSize{41, 63},
-    CardSize{70, 70},
-    CardSize{70, 110},
+    CardSize{43, 65},
+    CardSize{45, 68},
+    CardSize{49, 93},
+    CardSize{50, 75},
+    CardSize{54, 80},
+    CardSize{55, 80},
+    CardSize{56, 87},
+    CardSize{57, 89},
+    CardSize{57.5, 89},
+    CardSize{59, 92},
+    CardSize{61, 103},
+    CardSize{61, 112},
+    CardSize{63.5, 88},
+    CardSize{63.5, 92},
     CardSize{65, 100},
     CardSize{65, 110},
-    CardSize{61, 112}
+    CardSize{70, 70},
+    CardSize{70, 110},
+    CardSize{70, 120},
+    CardSize{75, 105},
+    CardSize{80, 120},
+    CardSize{80, 80},
+    CardSize{87, 112},
+    CardSize{88, 125}
 };
 
 #endif // CARDSIZESCONSTANTS_HPP
