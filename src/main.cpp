858f57f23284f6cc0a5e96fa24c8b6a79a52f6d5
okotik <zbut.links@gmail.com>
2019-07-04 23:16:23 +0300

    Delete src/src.pro

--- src/main.cpp
+++ src/main.cpp
@@ -6,15 +6,15 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
 
-    QStandardItemModel model;
+//    QStandardItemModel model;
 
-    model.appendRow(new QStandardItem(QIcon(":/new/preview/images/project"), "item1"));
-    model.appendRow(new QStandardItem(QIcon(":/new/preview/images/project"), "item2"));
-    model.appendRow(new QStandardItem(QIcon(":/new/preview/images/project"), "item3"));
+//    model.appendRow(new QStandardItem(QIcon(":/new/preview/images/project"), "item1"));
+//    model.appendRow(new QStandardItem(QIcon(":/new/preview/images/project"), "item2"));
+//    model.appendRow(new QStandardItem(QIcon(":/new/preview/images/project"), "item3"));
 
-    PrototypeComponentsNavigatorWindow window;
+//    PrototypeComponentsNavigatorWindow window;
 
-    window.setModel(&model);
-    window.show();
+//    window.setModel(&model);
+//    window.show();
     return a.exec();
 }
