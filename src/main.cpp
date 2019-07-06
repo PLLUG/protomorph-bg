64490fdac4f7533dbfd42db1e7d2a9eaaff712c7
okotik <zbut.links@gmail.com>
2019-07-06 11:06:24 +0300

    Revert "Test merge"
    This reverts commit 2baeb46b9380d920c5222a95a77d9ac5ef4b052b, reversing
    changes made to 858f57f23284f6cc0a5e96fa24c8b6a79a52f6d5.

--- src/main.cpp
+++ src/main.cpp
@@ -2,17 +2,10 @@
 #include <QStandardItemModel>
 #include "prototypecomponentsnavigatorwindow.h"
 
-#include "recentproject.h"
-#include "programsettings.h"
-#include "projectviewerwindow.h"
-#include "recentprojectsmodel.h"
-
-
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
 
-<<<<<<< HEAD
 //    QStandardItemModel model;
 
 //    model.appendRow(new QStandardItem(QIcon(":/new/preview/images/project"), "item1"));
@@ -23,20 +16,5 @@ int main(int argc, char *argv[])
 
 //    window.setModel(&model);
 //    window.show();
-=======
-    RecentProject pr1("/home/game1");
-    RecentProject pr2("/home/igor/bla/blabla/game2");
-    RecentProject pr3("/home/igor/Documents/game3");
-
-    QVector<RecentProject> projects{pr1,pr2,pr3};
-
-    ProgramSettings settings{projects};
-
-    RecentProjectsModel model(settings);
-
-    ProjectViewerWindow *pvw = new ProjectViewerWindow;
-    pvw->setModel(&model);
-    pvw->show();
->>>>>>> development
     return a.exec();
 }
--- src/tests/projectviewerwindow/tests.pro
+++
@@ -1,9 +0,0 @@
-QT += testlib
-QT += gui widgets
-CONFIG += qt warn_on depend_includepath testcase
-
-TEMPLATE = app
-
-SOURCES +=  tst_projectviewerwindow.cpp
-
-include($$PWD/../../projectviewer/projectviewer.pri)
