726c849ca05f65d1fd38489c3c78dc850404d3c9
okotik <zbut.links@gmail.com>
2019-08-04 15:42:43 +0300

    Delete sum comments

--- src/boardgameprojectfile/boardgameprojectfile.cpp
+++ src/boardgameprojectfile/boardgameprojectfile.cpp
@@ -6,7 +6,7 @@
 
 std::unique_ptr<BoardGame> BoardGameProjectFile::load(const QString &path)
 {
-
+    //this method is intentionally empty and will be implemented later on
 }
 
 void BoardGameProjectFile::save(const QString &path)
@@ -16,25 +16,11 @@ void BoardGameProjectFile::save(const QString &path)
 
     // First, lets serialize some description.
     auto descriptionOne = builder.CreateString("Orest");
-    //    auto descriptionTwo = builder.CreateString("Man");
 
     // Use the `CreateBoardGame` shortcut to create BoardGame with all fields set.
     auto orest = CreateBoardGame(builder, descriptionOne);
-    //    auto man = CreateBoardGame(builder, descriptionTwo);
-
-    //    // Create a FlatBuffer's `vector` from the `std::vector`.
-    //    std::vector<flatbuffers::Offset<BoardGame>> boardGame_vector;
-    //    boardGame_vector.push_back(orest);
-    //    boardGame_vector.push_back(man);
-    //    auto boardGame = builder.CreateVector(boardGame_vector);
-
-    //     // Shortcut for creating BoardGame with all fields set:
-
-    //    auto game = CreateBoardGame(builder, boardGame);
 
     builder.Finish(orest); // Serialize the root of the object.
-    //    builder.GetBufferPointer();
-    //    builder.GetSize();
 
     QFile file("file.bgame");
     if(file.open(QIODevice::WriteOnly))
